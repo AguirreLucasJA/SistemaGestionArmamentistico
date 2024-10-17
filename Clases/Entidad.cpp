f4517ce9a2d994ee7b2121135a4a6f63394000eb
Lucas Aguirre <109088200+AguirreLucasJA@users.noreply.github.com>
2024-10-17 14:41:39 -0300

    codeo Entidad.h
    Termine de codear entidad.h

--- Clases/Entidad.cpp
+++ Clases/Entidad.cpp
@@ -1,13 +1,1 @@
-//Nombre: Juan Sebastian Kalksma
-//TP N°:
-//EJ N°:
-//Consigna:
-
-#include<iostream>
-using namespace std;
-//#include <string> //Libreria de string
-
-int main(){
-
-         return 0;
-}
+#include "Menu.h"
--- Clases/Entidad.h
+++ Clases/Entidad.h
@@ -1,6 +1,30 @@
-#ifndef ENTIDAD_H_INCLUDED
-#define ENTIDAD_H_INCLUDED
+#pragma once
 
+class Entidad
+{
 
+public:
+    Entidad() {};
 
-#endif // ENTIDAD_H_INCLUDED
+    void setUsuario(char* _Usuario);
+    void setClave(char* _Clave);
+    void setDescripcion(char* _Descripcion);
+    void setTipoUsuario(int _tipoUsuario);
+    void setEstado(bool _Estado);
+    void setId(int _Id);
+
+    char* getUsuario();
+    char* getClave();
+    char* getDescripcion();
+    int getTipoUsuario();
+    bool getEstado();
+    int getId();
+
+private:
+    char Usuario[30];
+    char Clave[30];
+    char Descripcion[30];
+    int TipoUsuario;
+    bool Estado;
+    int Id;
+};
