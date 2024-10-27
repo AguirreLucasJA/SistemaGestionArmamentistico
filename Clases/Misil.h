f9c8958c58157ca0d8ffa723abf23365f74b102c
Lucas Aguirre <109088200+AguirreLucasJA@users.noreply.github.com>
2024-10-27 13:38:07 -0300

    codeo clase

--- Clases/Misil.h
+++ Clases/Misil.h
@@ -6,7 +6,7 @@ class Misil : public Producto
 {
 
 public:
-    Misil(){};
+    Misil() {};
 
     void setCantCabezasExplosivas(int cantCabezasExplosivas);
     void setVelocidadMax(int velocidadMax);
@@ -20,19 +20,31 @@ public:
     void setHipersonico(bool hipersonico);
     void setEstado(bool estado);
 
+    int getCantCabezasExplosivas();
+    int getVelocidadMax();
+    int getPeso();
+    int getCodigoUnico();
+    std::string getTipoCombustible();
+    std::string getTipoCarga();
+    std::string getTipo();
+    std::string getTipoPropulsion();
+    std::string getTipoGuia();
+    bool gethipersonico();
+    bool getEstado();
 
+    void cargar();
+    void mostrar();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+private:
+    int _cantCabezasExplosivas;
+    int _velocidadMax;
+    int _peso;
+    int _codigoUnico
+    char _tipoCombustible[30];
+    char _tipoCarga[30];
+    char _tipo[30];
+    char _tipoPropulsion[30];
+    char _tipoGuia[30];
+    bool _hipersonico;
+    bool _estado;
 };
