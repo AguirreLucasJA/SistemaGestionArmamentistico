4b05751a247d831a5b4841afc7761042eae5302f
Lucas Aguirre <109088200+AguirreLucasJA@users.noreply.github.com>
2024-11-12 16:21:14 -0300

    fix Archi admin get nuevo ID

--- Clases/ClasesArchivos/ArchivoAdmin.cpp
+++ Clases/ClasesArchivos/ArchivoAdmin.cpp
@@ -142,11 +142,32 @@ bool ArchivoAdmin::leerTodos(Admin *vecRegistros, int cantidadReg)
 
 int ArchivoAdmin::getNuevoId()
 {
-    int nuevoId;
+    FILE *pFile;
+    Admin registro;
+    int nuevoId = 0;
 
-    nuevoId =  getCantidadReg();
+    pFile = fopen(_nombreArchivo.c_str(), "rb");
 
-    return ++ nuevoId;
+    if(pFile == nullptr)
+    {
+        return 1; //si no pudo abrir el archivo
+    }
+
+    fseek(pFile, - _tamReg, SEEK_END);
+
+    if (fread(&registro, _tamReg, 1, pFile) == 1)
+    {
+        nuevoId = registro.getId() +1;
+    }
+
+    else
+    {
+        nuevoId = 1; // Si el archivo está vacío / no se pudo leerlo
+    }
+
+    fclose(pFile);
+
+    return nuevoId;
 }
 
 int ArchivoAdmin::buscarXUsuario(std::string usuario)
