a0e273852641b8eceb381fe098c7ecb04a2be955
Lucas Aguirre <109088200+AguirreLucasJA@users.noreply.github.com>
2024-11-15 15:31:38 -0300

    declaracion de archivos

--- Clases/ClasesArchivos/ArchivoAvion.h
+++ Clases/ClasesArchivos/ArchivoAvion.h
@@ -1,1 +1,23 @@
+#pragma once
+#include <string>
+#include "../Avion.h"
 
+
+
+class ArchivoAvion
+{
+public:
+    ArchivoAvion(); //construye con nombre del Archivo y tamaño del REG.
+    bool guardar(const Avion &registro);//Agrega un Avion
+    bool guardar(const Avion &registro, int pos);//Modifica un Avion en una posicion
+    int buscarXId(int id);//busca un Avion por su id
+    int getCantidadReg();//Devuelve cantidad de Registros
+    Avion leer(int pos);//Lee un Avion en una posicion
+    bool leerTodos(Avion *vecRegistros, int cantidadReg);//Lee todos
+    int getNuevoId();//Devuelve id AUTONUMERICO
+    int buscarXNombre(std::string nombre);
+
+private:
+    std::string _nombreArchivo;
+    int _tamReg;
+};
--- Clases/ClasesArchivos/ArchivoBuque.h
+++ Clases/ClasesArchivos/ArchivoBuque.h
@@ -1,1 +1,23 @@
+#pragma once
+#include <string>
+#include "../Buque.h"
 
+
+
+class ArchivoBuque
+{
+public:
+    ArchivoBuque(); //construye con nombre del Archivo y tamaño del REG.
+    bool guardar(const Buque &registro);//Agrega un Buque
+    bool guardar(const Buque &registro, int pos);//Modifica un Buque en una posicion
+    int buscarXId(int id);//busca un Buque por su id
+    int getCantidadReg();//Devuelve cantidad de Registros
+    Buque leer(int pos);//Lee un Buque en una posicion
+    bool leerTodos(Buque *vecRegistros, int cantidadReg);//Lee todos
+    int getNuevoId();//Devuelve id AUTONUMERICO
+    int buscarXNombre(std::string nombre);
+
+private:
+    std::string _nombreArchivo;
+    int _tamReg;
+};
